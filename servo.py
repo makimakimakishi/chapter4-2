5d3dceff4533717c9b7eb8d0891e645553ee70cb
makimakimakishi <91859293+makimakimakishi@users.noreply.github.com>
2021-10-25 18:39:31 +0900

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,4 +1,3 @@
-import wiringpi
 import time
 import RPi.GPIO as GPIO
 import sys
@@ -11,11 +10,4 @@ middle = 7.25 #   0deg : 1.45ms / 20ms = 7.25[%]
 top = 12.0    #  90deg : 2.4ms / 20ms = 12.0[%]
 param = sys.argv
 set_degree =int(param[1])
-wiringpi.wiringPiSetupGpio()
-wiringpi.pinMode( servo_pin, 2 )
-wiringpi.pwmSetMode(0)
-wiringpi.pwmSetRange(1024)
-wiringpi.pwmSetClock(375)
-move_deg = int( 81 + 41 / 90 * set_degree )
-wiringpi.pwmWrite( 2,move_deg )
-
+servo.ChangeDutyCycle(95*set_degree/1800+7.25)
