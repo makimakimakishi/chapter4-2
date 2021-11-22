a6a0ff6e5f006cf7b27f4b7651a5ae1aeccd02fc
makimakimakishi <91859293+makimakimakishi@users.noreply.github.com>
2021-11-22 11:11:08 +0900

    Update servo.py

--- servo.py
+++ servo.py
@@ -2,8 +2,8 @@ import time
 import RPi.GPIO as GPIO
 import sys
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(2, GPIO.OUT)
-servo = GPIO.PWM(2, 50)
+GPIO.setup(3, GPIO.OUT)
+servo = GPIO.PWM(3, 50)
 servo.start(0.0)
 bottom = 2.5  # -90deg : 0.5ms / 20ms = 2.5[%]
 middle = 7.25 #   0deg : 1.45ms / 20ms = 7.25[%]
