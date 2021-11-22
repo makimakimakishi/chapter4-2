054809cce56d2b71f41b0e179c0c9a16a08776e4
makimakimakishi <91859293+makimakimakishi@users.noreply.github.com>
2021-11-22 10:50:30 +0900

    Update servo.py

--- servo.py
+++ servo.py
@@ -9,13 +9,16 @@ bottom = 2.5  # -90deg : 0.5ms / 20ms = 2.5[%]
 middle = 7.25 #   0deg : 1.45ms / 20ms = 7.25[%]
 top = 12.0    #  90deg : 2.4ms / 20ms = 12.0[%]
 param = sys.argv
-set_degree =int(param[1])
-print(set_degree)
-s=95*set_degree
-v=s/1800
-c=v+7.25
-servo.ChangeDutyCycle(c)
-for i in range(5):
+set_degree =int(param[1]):
+setservo(setdegree)
+
+def setservo(setdegree)
+    print(set_degree)
+    s=95*set_degree
+    v=s/1800
+    c=v+7.25
+    servo.ChangeDutyCycle(c)
+    for i in range(5):
 	servo.ChangeDutyCycle(bottom)
 	time.sleep(1.0)
 
